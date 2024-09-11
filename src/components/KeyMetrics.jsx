02b8dda9c96c25e726d03e49598bca0d00eb5e7b
michellemtchai <michellemtchai@gmail.com>
2024-09-11 05:38:56 -0400

    fix spacing for key metrics

--- src/components/KeyMetrics.jsx
+++ src/components/KeyMetrics.jsx
@@ -4,7 +4,7 @@ import MetricEntry from './MetricEntry';
 function KeyMetrics(props) {
   return (
     <Section title="Key Metrics">
-      <ul className="flex flex-row justify-between mx-4">
+      <ul className="flex flex-row justify-between mx-4 my-2">
         <MetricEntry
           label="Active Users"
           value={`${props.activeUsers}/${props.totalUsers}`}
